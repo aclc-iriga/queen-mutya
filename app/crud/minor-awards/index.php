155e1c2201fb6fa99bafb1c6b4ebad7e0547b3fc
kulotsystems <kulotsystems@gmail.com>
2023-05-25 15:16:10 +0800

    Update title of minor-awards page.

--- app/crud/minor-awards/index.php
+++ app/crud/minor-awards/index.php
@@ -36,7 +36,7 @@
 </head>
 <body>
     <div class="container py-5">
-        <h2 class="fw-bolder">Binibining San Vicente (Buhi, Camarines Sur) 2023</h2>
+        <h2 class="fw-bolder">Mr. and Ms. San Agustin (Iriga City) 2023</h2>
     </div>
     <div>
         <table>
