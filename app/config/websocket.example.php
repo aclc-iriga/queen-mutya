5f7513cbc9983f9603a9ccbacde01e8e2f4f50f3
Arvic S. Babol <kulotsystems@gmail.com>
2025-06-22 16:00:16 +0800

    Update websocket.example.php.

--- app/config/websocket.example.php
+++ app/config/websocket.example.php
@@ -2,8 +2,14 @@
 
 /*
 |-----------------------------------------------------------------------
-| Websocket Configuration
+| WebSocket Configuration
 |-----------------------------------------------------------------------
+| Enter the configuration for your WebSocket server
+|
+|    url  ->  WebSocket server URL
+|             Use port 8079 if the server is started with --dev mode
+|             Example: 'ws://127.0.0.1:8079'
+|
 */
 
 $websocket = [
